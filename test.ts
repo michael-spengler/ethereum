03a348551e50335470ebedbac8f1630d2efd23ea
Michael Spengler <mycamper@gmx.de>
2020-08-23 23:56:22 +0200

    improved test as documentation

--- test.ts
+++ test.ts
@@ -1,11 +1,13 @@
 import { fail, assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { EtherscanAPI } from "./etherscan-api.ts";
+// import { apiKey } from "./.env.ts";
+
 
 Deno.test("getEtherBalanceForASingleAddress", async (): Promise<void> => {
 
-    const response = await EtherscanAPI.getEtherBalanceForASingleAddress('0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a', 'thisIsAnInvalidToken')
+    const response = await EtherscanAPI.getEtherBalanceForASingleAddress('0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a', 'apiKey')
 
-    assertEquals('Invalid API Key', response.result)
+    assertEquals('Invalid API Key', response.result) // because we are testing with an invalid api key
 
 });
 
